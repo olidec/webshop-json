c7d56b36d1edc0951094444d11aa898fec3e39b1
olidec <69003898+olidec@users.noreply.github.com>
2024-02-02 08:22:30 +0100

    edits

--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
 import { jsonLoader } from "./utils"
 
 function render(data, targetElement) {
+  let priceClass = "price"
+  let reducedText = ""
+
+  if (!data.description) { 
+    data.description = "[keine Beschreibung]"
+  }
+
+  if (data.reducedPrice) {
+    data.price = data.reducedPrice
+    priceClass += " reduced"
+    reducedText = "<span class='reduced-text'>Aktion: </span>"
+  }
+
+
+
   targetElement.innerHTML += `
 <div class="article">
     <h3>${data.title}</h3>
     <div class="description">
         ${data.description}
+    </div>
+    <div class="${priceClass}"> 
+        ${reducedText}${data.price}
     </div>
 </div>
 `
@@ -14,7 +32,7 @@ function render(data, targetElement) {
 async function main() {
   const artList = document.getElementById("article-list")
 
-  const data = await jsonLoader("/level-01.json")
+  const data = await jsonLoader("/level-03.json")
 
   for (let i = 0; i < data.length; i++) {
     render(data[i], artList)
